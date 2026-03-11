454ec3619298ecd283d11ec57150fd2d03965dff
Snasn3n <jonasneugebauer.01@gmx.de>
2026-03-11 18:09:19 +0100

    added getters and setters

--- Note.java
+++ Note.java
@@ -1,3 +1,37 @@
+import java.time.LocalDateTime;
+
 public class Note {
-    
+    private LocalDateTime date;
+    private String title;
+    private String content;
+
+
+    public Note(LocalDateTime date, String title, String content) {
+        this.date = date;
+        this.title = title;
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
