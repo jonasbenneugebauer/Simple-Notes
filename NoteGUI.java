17e7545b7fbf3f2c318969b45327d6249c3d74ad
Snasn3n <jonasneugebauer.01@gmx.de>
2026-03-12 19:44:39 +0100

    working frame in gui

--- NoteGUI.java
+++ NoteGUI.java
@@ -1,9 +1,15 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.JScrollPane;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 public class NoteGUI {
     
 private JList list;
@@ -15,25 +21,47 @@ private JButton editButton;
 private JFrame frame;
 
 
+
 public NoteGUI() {
     this.list = new JList();
     this.field = new JTextField();
     this.area = new JTextArea();
+    JScrollPane scrollPane = new JScrollPane(area);
     this.addButton = new JButton("Add");
     this.deleteButton = new JButton("Delete");
     this.editButton = new JButton("Edit");
     this.frame = new JFrame("Simple Notes");
 
+    frame.setLayout(new BorderLayout());
+
 
     frame.setSize(1080, 720);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  
 
-    frame.add(list);
-    frame.add(field);
-    frame.add(area);
-    frame.add(addButton);
-    frame.add(deleteButton);
-    frame.add(editButton);
+    JPanel rightPanel = new JPanel();
+    rightPanel.setLayout(new BorderLayout());
+    rightPanel.add(field, BorderLayout.NORTH);
+    rightPanel.add(scrollPane, BorderLayout.CENTER);
+
+    JPanel buttonPanel = new JPanel();
+    buttonPanel.add(addButton);
+    buttonPanel.add(deleteButton);
+    buttonPanel.add(editButton);
+
+    rightPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+    JLabel titleLabel = new JLabel("Title:");
+    JPanel titlePanel = new JPanel(new BorderLayout());
+    titlePanel.add(titleLabel, BorderLayout.NORTH);
+    titlePanel.add(field, BorderLayout.CENTER);
+
+    rightPanel.add(titlePanel, BorderLayout.NORTH);
+
+    frame.add(list, BorderLayout.WEST);
+    frame.add(rightPanel, BorderLayout.CENTER);
+
+    list.setPreferredSize(new Dimension(300, 0));
 
     frame.setVisible(true);
 }
