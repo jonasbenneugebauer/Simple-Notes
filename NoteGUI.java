0f4b65588231e490cda0f5e021a0271095b4450b
Snasn3n <jonasneugebauer.01@gmx.de>
2026-03-12 11:34:56 +0100

    added attributes for GUI

--- NoteGUI.java
+++ NoteGUI.java
@@ -1,3 +1,27 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class NoteGUI {
     
+private JList list;
+private JTextField field;
+private JTextArea area;
+private JButton addButton;
+private JButton deleteButton;
+private JButton editButton;
+private JFrame frame;
+
+
+public NoteGUI() {
+    this.list = new JList();
+    this.field = new JTextField();
+    this.area = new JTextArea();
+    this.addButton = new JButton("Add");
+    this.deleteButton = new JButton("Delete");
+    this.editButton = new JButton("Edit");
+    this.frame = new JFrame("Simple Notes");
+}
 }
