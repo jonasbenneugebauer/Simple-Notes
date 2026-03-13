bbc453244e7dd808552c19c2b5addd26620b5e2b
Snasn3n <jonasneugebauer.01@gmx.de>
2026-03-13 20:09:23 +0100

    added NoteManager

--- NoteManager.java
+++ NoteManager.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+
 public class NoteManager {
-        
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    public void addNote(Note note) {
+        notes.add(note);
+    }
+
+    public void deleteNote(int index) {
+        notes.remove(index);
+    }
+
+    public ArrayList<Note> getNotes() {
+        return notes;
+    }
+
+    public void editNote(int index, String newTitle, String newContent){
+        Note note = notes.get(index);
+        note.setTitle(newTitle);
+        note.setContent(newContent);
+    }
+
+    public Note getNote(int index) {
+        return notes.get(index);
+    }
+
 }
